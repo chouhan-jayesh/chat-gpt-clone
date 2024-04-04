9e4b60b0f92d03090c0dae475f8e2c381d9d52df
Jayesh Chouhan <jayeshchouhan445@gmail.com>
2024-04-04 19:31:29 +0530

    embedded boiler plate

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen bg-black">
+        <section>
+            <Header />
+        </section>
+    </div>
+  );
+}
